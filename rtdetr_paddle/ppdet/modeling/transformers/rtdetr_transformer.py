52252668bb93f259b67958ee9a4f58e2c8575762
lyuwenyu <wenyu.lyu@gmail.com>
2023-06-27 14:44:34 +0800

    init

--- rtdetr_paddle/ppdet/modeling/transformers/rtdetr_transformer.py
+++ rtdetr_paddle/ppdet/modeling/transformers/rtdetr_transformer.py
@@ -1,26 +1,3 @@
-# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Modified from Deformable-DETR (https://github.com/fundamentalvision/Deformable-DETR)
-# Copyright (c) 2020 SenseTime. All Rights Reserved.
-# Modified from detrex (https://github.com/IDEA-Research/detrex)
-# Copyright 2022 The IDEA Authors. All rights reserved.
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import paddle
 import paddle.nn as nn
 import paddle.nn.functional as F
