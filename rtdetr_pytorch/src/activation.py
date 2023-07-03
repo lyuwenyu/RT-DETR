e8185cbc3e9a6f6dd7c50336adc3a2e75cbe7344
lyuwenyu <wenyu.lyu@gmail.com>
2023-07-03 10:13:38 +0800

    add resnet

---
+++ rtdetr_pytorch/src/activation.py
@@ -0,0 +1,37 @@
+import torch 
+import torch.nn as nn
+
+
+def get_activation(act: str, inpace: bool=True):
+    '''get activation
+    '''
+    act = act.lower()
+    
+    if act == 'silu':
+        m = nn.SiLU()
+
+    elif act == 'relu':
+        m = nn.ReLU()
+
+    elif act == 'leaky_relu':
+        m = nn.LeakyReLU()
+
+    elif act == 'silu':
+        m = nn.SiLU()
+    
+    elif act == 'gelu':
+        m = nn.GELU()
+        
+    elif act is None:
+        m = nn.Identity()
+    
+    elif isinstance(act, nn.Module):
+        m = act
+
+    else:
+        raise RuntimeError('')  
+
+    if hasattr(m, 'inplace'):
+        m.inplace = inpace
+    
+    return m 
