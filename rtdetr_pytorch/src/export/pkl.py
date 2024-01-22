8606744c2ab208c939642d17e2a55eae780f06df
Aaron <aaronrmm@gmail.com>
2024-01-22 09:40:43 -0500

    add torch pkl export

---
+++ rtdetr_pytorch/src/export/pkl.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+import torch
+import torch.nn as nn
+from src.core import YAMLConfig
+
+
+class Model(nn.Module):
+    def __init__(self, cfg) -> None:
+        super().__init__()
+        self.model = cfg.model.deploy()
+        self.postprocessor = cfg.postprocessor.deploy()
+        print(self.postprocessor.deploy_mode)
+
+    def forward(self, images, orig_target_sizes):
+        outputs = self.model(images)
+        print("POSTPROCESSING OUTPUT")
+        return self.postprocessor(outputs, orig_target_sizes)
+
+
+def export_torch_pkl(cfg: YAMLConfig, checkpoint_path: Path, output_path: Path):
+    checkpoint = torch.load(checkpoint_path, map_location="cpu")
+    if "ema" in checkpoint:
+        state = checkpoint["ema"]["module"]
+    else:
+        state = checkpoint["model"]
+
+    # NOTE load train mode state -> convert to deploy mode
+    cfg.model.load_state_dict(state)
+
+    model = Model(cfg)
+    torch.save(model, output_path)
