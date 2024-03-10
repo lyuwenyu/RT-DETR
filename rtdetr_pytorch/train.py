8010d7f293a91adde19422fe8b531e2bbafd9df4
max-unfinity <poldersen2@gmail.com>
2024-03-10 01:41:53 +0000

    fix FileNotFoundError

--- rtdetr_pytorch/train.py
+++ rtdetr_pytorch/train.py
@@ -12,6 +12,7 @@ def train(model: str, finetune: bool, config_path: str):
         name = os.path.basename(checkpoint_url)
         checkpoint_path = f"models/{name}"
         if not os.path.exists(checkpoint_path):
+            os.makedirs("models", exist_ok=True)
             torch.hub.download_url_to_file(checkpoint_url, checkpoint_path)
         tuning = checkpoint_path
     else:
