4d7d23fea6a19e4398f773a526a8c555563e001c
max-unfinity <poldersen2@gmail.com>
2024-03-27 12:14:32 +0000

    benchmarking

--- benchmark_onnx.sh
+++ benchmark_onnx.sh
@@ -1,5 +1,5 @@
 models=()
-# models+=("models/rtdetr_r18vd_dec3_6x_coco_from_paddle.pth")
+models+=("models/rtdetr_r18vd_dec3_6x_coco_from_paddle.pth")
 models+=("models/rtdetr_r34vd_dec4_6x_coco_from_paddle.pth")
 models+=("models/rtdetr_r50vd_6x_coco_from_paddle.pth")
 models+=("models/rtdetr_r50vd_m_6x_coco_from_paddle.pth")
