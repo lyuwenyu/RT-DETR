4f95880da8a537e090cdd9f3e9dc781eb9c024c5
max-unfinity <poldersen2@gmail.com>
2024-03-27 11:13:29 +0000

    bash onnx script

---
+++ benchmark_onnx.sh
@@ -0,0 +1,11 @@
+models=()
+# models+=("models/rtdetr_r18vd_dec3_6x_coco_from_paddle.pth")
+models+=("models/rtdetr_r34vd_dec4_6x_coco_from_paddle.pth")
+models+=("models/rtdetr_r50vd_6x_coco_from_paddle.pth")
+models+=("models/rtdetr_r50vd_m_6x_coco_from_paddle.pth")
+models+=("models/rtdetr_r101vd_6x_coco_from_paddle.pth")
+
+
+for model in "${models[@]}"; do
+    python benchmark_onnx.py --checkpoint $model
+done
