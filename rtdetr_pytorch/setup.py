0914d3ea5402d3488cd097c52e18d2e53e8338f8
Aaron <aaronrmm@gmail.com>
2024-05-28 10:45:46 -0400

    rename package to rtrdetr

--- rtdetr_pytorch/setup.py
+++ rtdetr_pytorch/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='RT-ODETR',
+    name='rtrdetr',
     version='0.1.0',
     author='Your Name',
     author_email='your.email@example.com',
