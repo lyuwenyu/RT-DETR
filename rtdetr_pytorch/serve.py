f05dfbec1da259613cfe743b8f90088b91627f3b
unfinity <poldersen2@gmail.com>
2024-06-10 20:59:37 +0000

    predict_benchmark + better runtimes

--- rtdetr_pytorch/serve.py
+++ rtdetr_pytorch/serve.py
@@ -1,9 +1,8 @@
 import os
-import time
 import yaml
 import numpy as np
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Tuple
 from PIL import Image, ImageOps
 import torch
 from torchvision.transforms import ToTensor
@@ -12,6 +11,7 @@ import onnxruntime
 from dotenv import load_dotenv
 import supervisely as sly
 from supervisely.nn.prediction_dto import PredictionBBox
+from supervisely.nn.inference import Timer
 from supervisely.app.widgets import (
     Widget,
     PretrainedModelsSelector,
@@ -35,6 +35,131 @@ if sly.is_development():
     load_dotenv("local.env")
     load_dotenv(os.path.expanduser("~/supervisely.env"))
 root_dir = Path(__file__).parent.parent
+
+
+class PyTorchInference:
+    def __init__(self, checkpoint_path, config_path, device, img_size, class_names):
+        self.checkpoint_path = checkpoint_path
+        self.config_path = config_path
+        self.device = device
+        self.img_size = img_size
+        self.class_names = class_names
+
+    def load_model(self):
+        # TODO: why device is not used
+        cfg = YAMLConfig(
+            self.config_path,
+            resume=self.checkpoint_path,
+            tuning='',
+        )
+        # TODO: Why is eval_spatial_size not set while training?
+        cfg.yaml_cfg["HybridEncoder"]["eval_spatial_size"] = self.img_size
+        cfg.yaml_cfg["RTDETRTransformer"]["eval_spatial_size"] = self.img_size
+        solver = DetSolver(cfg)
+        solver.setup()
+        solver.resume(solver.cfg.resume)
+        self.solver = solver
+        self.model = solver.ema.module if solver.is_ema_loaded else solver.model
+        self.model.eval()
+        self.model.to(self.device)
+        self.transform = Compose(ops=[
+            Resize(self.img_size),
+            ToImageTensor(),
+            ConvertDtype()
+        ])
+    
+    def predict_benchmark(self, image_path: str, settings: dict = None):
+        # 1. Preprocess
+        with Timer() as preprocess_timer:
+            img = read_image(image_path)
+            w, h = img.size
+            orig_target_sizes = torch.as_tensor([int(w), int(h)]).unsqueeze(0)
+            orig_target_sizes = orig_target_sizes.to(self.device)
+            samples = self.transform(img)[None]
+            samples = samples.to(self.device)
+        
+        # 2. Inference
+        with Timer() as inference_timer:
+            with torch.no_grad():
+                outputs = self.model(samples)
+        
+        # 3. Postprocess
+        with Timer() as postprocess_timer:
+            postprocessors = self.solver.postprocessor
+            results = postprocessors(outputs, orig_target_sizes)
+            results = results[0]
+            if not postprocessors.remap_mscoco_category:
+                classes = [self.class_names[i] for i in results["labels"].cpu().numpy()]
+            else:
+                classes = [mscoco_category2name[i] for i in results["labels"].cpu().numpy()]
+            boxes = results["boxes"].cpu().numpy()
+            scores = results["scores"].cpu().numpy()
+            conf_tresh = settings.get("confidence_thresh", DEFAULT_CONF)
+            predictions = format_predictions(classes, boxes, scores, conf_tresh)
+
+        benchmark = {
+            "preprocess": preprocess_timer.get_time(),
+            "inference": inference_timer.get_time(),
+            "postprocess": postprocess_timer.get_time(),
+        }
+        return predictions, benchmark
+    
+    def shutdown(self):
+        del self.model
+        del self.solver
+
+
+class ONNXInference:
+    def __init__(self, onnx_model_path, device, img_size, class_names):
+        self.onnx_model_path = onnx_model_path
+        self.device = device
+        self.img_size = list(img_size)
+        self.class_names = class_names
+
+    def load_model(self):
+        if self.device == "cpu":
+            providers = ["CPUExecutionProvider"]
+        else:
+            assert torch.cuda.is_available(), "CUDA is not available"
+            providers = ["CUDAExecutionProvider"]
+        self.onnx_session = onnxruntime.InferenceSession(self.onnx_model_path, providers=providers)
+    
+    def predict_benchmark(self, image_path: str, settings: dict = None):
+        # 1. Preprocess
+        with Timer() as preprocess_timer:
+            img = read_image(image_path)
+            w, h = img.size
+            img_input, size_input = self._prepare_image(img, self.img_size)
+
+        # 2. Inference
+        with Timer() as inference_timer:
+            labels, boxes, scores = self.onnx_session.run(output_names=None, input_feed={'images': img_input, "orig_target_sizes": size_input})
+        
+        # 3. Postprocess
+        with Timer() as postprocess_timer:
+            labels, boxes, scores = labels[0], boxes[0], scores[0]
+            boxes_orig = boxes / np.array(self.img_size * 2) * np.array([w, h, w, h])
+            classes = [self.class_names[label] for label in labels]
+            conf_tresh = settings.get("confidence_thresh", DEFAULT_CONF)
+            predictions = format_predictions(classes, boxes_orig, scores, conf_tresh)
+
+        benchmark = {
+            "preprocess": preprocess_timer.get_time(),
+            "inference": inference_timer.get_time(),
+            "postprocess": postprocess_timer.get_time(),
+        }
+        return predictions, benchmark
+
+    def shutdown(self):
+        # at the moment, onnxruntime does not have a method to release resources
+        # see https://github.com/microsoft/onnxruntime/issues/17142
+        del self.onnx_session
+
+    def _prepare_image(self, img: Image.Image, img_size: list):
+        img = img.resize(tuple(img_size))
+        img_tensor = ToTensor()(img)[None].numpy()
+        size = np.array([list(img_size)], dtype=int)
+        return img_tensor, size
 
 
 class RTDETR(sly.nn.inference.ObjectDetection):
@@ -100,13 +225,15 @@ class RTDETR(sly.nn.inference.ObjectDetection):
             raise ValueError("Both pretrained_model_idx and custom_checkpoint_path are None.")
         
         # 2. load model
-        if runtime == "PyTorch":
-            self._load_pytorch(checkpoint_path, config_path, device)
-        elif runtime == "ONNXRuntime":
-            # runtime = ONNX and weights is .pth
+        if self.runtime == "PyTorch":
+            self.pytorch_inference = PyTorchInference(checkpoint_path, config_path, device, self.img_size, self.class_names)
+            self.pytorch_inference.load_model()
+        elif self.runtime == "ONNXRuntime":
+            # when runtime is ONNX and weights is .pth
             from convert_onnx import convert_onnx
             onnx_model_path = convert_onnx(checkpoint_path, config_path)
-            self._load_onnx(onnx_model_path, device)
+            self.onnx_inference = ONNXInference(onnx_model_path, device, self.img_size, self.class_names)
+            self.onnx_inference.load_model()
         else:
             raise NotImplementedError()
         
@@ -152,35 +279,6 @@ class RTDETR(sly.nn.inference.ObjectDetection):
                 yaml.dump(config, f)
         return weights_dst_path, config_path
 
-    def _load_pytorch(self, checkpoint_path, config_path, device):
-        cfg = YAMLConfig(
-            config_path,
-            resume=checkpoint_path,
-            tuning='',
-        )
-        # TODO: Why is this not set while training?
-        cfg.yaml_cfg["HybridEncoder"]["eval_spatial_size"] = self.img_size
-        cfg.yaml_cfg["RTDETRTransformer"]["eval_spatial_size"] = self.img_size
-        solver = DetSolver(cfg)
-        solver.setup()
-        solver.resume(solver.cfg.resume)
-        self.solver = solver
-        self.model = solver.ema.module if solver.is_ema_loaded else solver.model
-        self.model.eval()
-        self.transform = Compose(ops=[
-            Resize(self.img_size),
-            ToImageTensor(),
-            ConvertDtype()
-        ])
-
-    def _load_onnx(self, onnx_model_path, device):
-        if device == "cpu":
-            providers = ["CPUExecutionProvider"]
-        else:
-            assert torch.cuda.is_available(), "CUDA is not available"
-            providers = ["CUDAExecutionProvider"]
-        self.onnx_session = onnxruntime.InferenceSession(onnx_model_path, providers=providers)
-
     def _load_meta_pretained_model(self, model_idx: int):
         model_dict = get_models()[model_idx]
         self.model_name = model_dict["name"]
@@ -204,66 +302,19 @@ class RTDETR(sly.nn.inference.ObjectDetection):
         self._model_meta = sly.ProjectMeta(obj_classes=sly.ObjClassCollection(obj_classes))
         self._get_confidence_tag_meta()
 
-    def predict(self, image_path: str, settings: dict) -> List[PredictionBBox]:
-        conf_tresh = settings.get("confidence_thresh", DEFAULT_CONF)
+    def predict_benchmark(self, image_path: str, settings: dict) -> Tuple[List[PredictionBBox], dict]:
         if self.runtime == "PyTorch":
-            classes, boxes, scores = self._predict_pytorch(image_path, settings)
+            predictions, benchmark = self.pytorch_inference.predict_benchmark(image_path, settings)
         elif self.runtime == "ONNXRuntime":
-            classes, boxes, scores = self._predict_onnx(image_path, settings)
+            predictions, benchmark = self.onnx_inference.predict_benchmark(image_path, settings)
         else:
             raise NotImplementedError()
-        predictions = self._format_predictions(classes, boxes, scores, conf_tresh)
-        return predictions
+        return predictions, benchmark
 
-    def _predict_pytorch(self, image_path: str, settings: dict = None):
-        img = self._read_image(image_path)
-        w, h = img.size
-        orig_target_sizes = torch.as_tensor([int(w), int(h)]).unsqueeze(0)
-        postprocessors = self.solver.postprocessor
-        with torch.no_grad():
-            samples = self.transform(img)[None]
-            samples = samples.to(self.device)
-            orig_target_sizes = orig_target_sizes.to(self.device)
-            outputs = self.model(samples)
-            results = postprocessors(outputs, orig_target_sizes)
-        results = results[0]
-        if not postprocessors.remap_mscoco_category:
-            classes = [self.class_names[i] for i in results["labels"].cpu().numpy()]
-        else:
-            classes = [mscoco_category2name[i] for i in results["labels"].cpu().numpy()]
-        boxes = results["boxes"].cpu().numpy()
-        scores = results["scores"].cpu().numpy()
-        return classes, boxes, scores
-    
-    def _predict_onnx(self, image_path: str, settings: dict = None):
-        img = self._read_image(image_path)
-        w, h = img.size
-        def prepare_image(img, img_size):
-            img = img.resize(tuple(img_size))
-            img_tensor = ToTensor()(img)[None].numpy()
-            size = np.array([list(img_size)], dtype=int)
-            return img_tensor, size
-        img_input, size_input = prepare_image(img, self.img_size)
-        ## Profile start
-        labels, boxes, scores = self.onnx_session.run(output_names=None, input_feed={'images': img_input, "orig_target_sizes": size_input})
-        ## Profile end
-        labels, boxes, scores = labels[0], boxes[0], scores[0]
-        boxes_orig = boxes / np.array(self.img_size*2) * np.array([w, h, w, h])
-        classes = [self.class_names[label] for label in labels]
-        return classes, boxes_orig, scores
-    
-    def _format_predictions(self, class_names, boxes: np.ndarray, scores, conf_tresh):
-        predictions = []
-        for class_name, bbox_xyxy, score in zip(class_names, boxes, scores):
-            if score < conf_tresh:
-                continue
-            bbox_xyxy = np.round(bbox_xyxy).astype(int)
-            bbox_xyxy = np.clip(bbox_xyxy, 0, None)
-            bbox_yxyx = [bbox_xyxy[1], bbox_xyxy[0], bbox_xyxy[3], bbox_xyxy[2]]
-            bbox_yxyx = list(map(int, bbox_yxyx))
-            predictions.append(PredictionBBox(class_name, bbox_yxyx, float(score)))
+    def predict(self, image_path: str, settings: dict) -> List[PredictionBBox]:
+        predictions, benchmark = self.predict_benchmark(image_path, settings)
         return predictions
-
+    
     def get_info(self):
         info = super().get_info()
         info["model_name"] = self.model_name
@@ -273,14 +324,37 @@ class RTDETR(sly.nn.inference.ObjectDetection):
 
     def get_classes(self) -> List[str]:
         return self.class_names
+    
+    def shutdown_model(self):
+        if self.runtime == "PyTorch":
+            self.pytorch_inference.shutdown()
+            del self.pytorch_inference
+        elif self.runtime == "ONNXRuntime":
+            self.onnx_inference.shutdown()
+            del self.onnx_inference
+        super().shutdown_model()
 
-    def _read_image(self, image_path: str):
-        img = Image.open(image_path).convert("RGB")
-        try:
-            img = ImageOps.exif_transpose(img)
-        except:
-            pass
-        return img
+
+def read_image(image_path):
+    img = Image.open(image_path).convert("RGB")
+    try:
+        img = ImageOps.exif_transpose(img)
+    except:
+        pass
+    return img
+
+
+def format_predictions(classes: list, boxes: np.ndarray, scores: list, conf_tresh: float):
+    predictions = []
+    for class_name, bbox_xyxy, score in zip(classes, boxes, scores):
+        if score < conf_tresh:
+            continue
+        bbox_xyxy = np.round(bbox_xyxy).astype(int)
+        bbox_xyxy = np.clip(bbox_xyxy, 0, None)
+        bbox_yxyx = [bbox_xyxy[1], bbox_xyxy[0], bbox_xyxy[3], bbox_xyxy[2]]
+        bbox_yxyx = list(map(int, bbox_yxyx))
+        predictions.append(PredictionBBox(class_name, bbox_yxyx, float(score)))
+    return predictions
 
 
 model = RTDETR(
