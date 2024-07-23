73485054b385fc76ce5486617043fd656beef469
manuel cuevas <manuel@thinkdeeply.com>
2024-07-23 11:01:20 -0700

    updated to use /rtrdetr-0.2-py3-none-any.whl s3

--- rtdetr_pytorch/setup.py
+++ rtdetr_pytorch/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rtrdetr',
-    version='0.1.0',
+    version='0.2',
     author='Your Name',
     author_email='your.email@example.com',
     description='A short description of the package',
