7a88a3aa1a0852d6c97208aa62d549176fdfc986
Darren Tsai <darrenjktsai@gmail.com>
2024-12-30 11:50:03 +1100

    chore: non-torchvision inference

--- rtdetrv2_pytorch/references/deploy/rtdetrv2_onnxruntime.py
+++ rtdetrv2_pytorch/references/deploy/rtdetrv2_onnxruntime.py
@@ -7,9 +7,9 @@ import torchvision.transforms as T
 import numpy as np 
 import onnxruntime as ort 
 from PIL import Image, ImageDraw
+import cv2
 
-
-def draw(images, labels, boxes, scores, thrh = 0.7):
+def draw(images, labels, boxes, scores, thrh = 0.3):
     for i, im in enumerate(images):
         draw = ImageDraw.Draw(im)
 
@@ -37,18 +37,23 @@ def main(args, ):
 
     im_pil = Image.open(args.im_file).convert('RGB')
     w, h = im_pil.size
-    orig_size = torch.tensor([w, h])[None]
-
-    transforms = T.Compose([
-        T.Resize((1280, 1280)),
-        T.ToTensor(),
-    ])
-    im_data = transforms(im_pil)[None]
+    
+    use_torchvision = False
+    if use_torchvision:
+        transforms = T.Compose([
+            T.Resize((1280, 1280)),
+            T.ToTensor(),
+        ])
+        im_data = transforms(im_pil)[None].data.numpy()
+    else:
+        # pil and numpy only
+        im_resized = im_pil.resize((1280,1280), Image.BILINEAR)
+        im_data_pil = np.transpose(np.array(im_resized).astype(np.float32) / 255.0, (2, 0, 1))  # Shape: (C, H, W)
+        im_data = np.expand_dims(im_data_pil, axis=0)  # Shape: (1, C, H, W)
 
     output = sess.run(
-        # output_names=['labels', 'boxes', 'scores'],
         output_names=None,
-        input_feed={'images': im_data.data.numpy(), "orig_target_sizes": orig_size.data.numpy()}
+        input_feed={'images': im_data, "orig_target_sizes": np.array([[w,h]])}
     )
 
     labels, boxes, scores = output
